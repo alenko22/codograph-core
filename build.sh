352857b183a718e1d7fbe87c2c30bcddf190f6b8
alenko22 <pulyatv1@gmail.com>
2026-04-15 13:42:29 +0500

    fixing bugs

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Обновляем список пакетов и устанавливаем Graphviz
 apt-get update
 apt-get install -y graphviz
-
-# Устанавливаем Python-зависимости
 pip install -r requirements.txt
+python manage.py collectstatic --noinput
